5dc6a84d0eedf832b74c09139816b1f5002106ab
NameLess <daffakusumamulia@gmail.com>
2024-04-15 19:10:01 +0700

    fix: Load with the new Simpanan.hpp

--- src/Load.hpp
+++ src/Load.hpp
@@ -7,15 +7,21 @@
 #include <vector>
 #include "Simpanan.hpp"
 #include "Exception.hpp"
+#include "Game.hpp"
 using namespace std;
+#define PB push_back
+#define ADV(arg1, arg2) fscanf(arg1, "%c", &arg2)
+#define FOR(i, a) for (int i = 0; i < a; i++)
 
 class Load{
     public:
+        static string ADVWORD(FILE*);
         static vector<Hewan> loadAnimal();
         static vector<Tanaman> loadPlant();
-        static vector<Item> loadMisc();
+        // static vector<Item> loadMisc();
         static vector<Produk> loadProduct();
         static vector<Bangunan> loadRecipe();
+        static Game loadState(string);
 };
 
 #endif
