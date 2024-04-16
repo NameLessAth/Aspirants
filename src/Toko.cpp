ff5328c62172b7159020a0b5895446b1e806bc0d
Kiza <themightyomegagd@gmail.com>
2024-04-16 18:23:32 +0700

    change simpanan to simpanan pointer

--- src/Toko.cpp
+++ src/Toko.cpp
@@ -1,28 +1,36 @@
 #include "Toko.hpp"
 
-vector<pair<Simpanan, int>> Toko::catalogue;
+vector<pair<Simpanan*, int>> Toko::catalogue;
 
 void Toko::insertItem(Simpanan item, int quantity){
     bool found = false;
     for(int i=0;i<Toko::catalogue.size();i++){
-        if(Toko::catalogue[i].first.getKode() == item.getKode()){
+        if(Toko::catalogue[i].first->getKode() == item.getKode()){
             found = true;
-            if(dynamic_cast<Bangunan*>(&Toko::catalogue[i].first)){
+            if(dynamic_cast<Bangunan*>(Toko::catalogue[i].first)){
                 Toko::catalogue[i].second += quantity;
             }
             break;
         }
     }
     if(!found){
-        Toko::catalogue.push_back(make_pair(item,quantity));
+        Toko::catalogue.push_back(make_pair(&item,quantity));
     }
+}
+
+int Toko::manyItem(){
+    return Toko::catalogue.size();
+}
+
+vector<pair<Simpanan*, int>> Toko::getCatalogue(){
+    return Toko::catalogue;
 }
 
 void Toko::printToko(){
     cout << "Selamat datang di toko!!\nBerikut merupakan hal yang dapat Anda Beli\n";
     for(int i=0;i<Toko::catalogue.size();i++){
-        cout << i+1 << ". " << Toko::catalogue[i].first.getNama() << " - " << Toko::catalogue[i].first.getHarga();
-        Simpanan* temp = &(Toko::catalogue[i].first);
+        cout << i+1 << ". " << Toko::catalogue[i].first->getNama() << " - " << Toko::catalogue[i].first->getHarga();
+        Simpanan* temp = (Toko::catalogue[i].first);
         if(dynamic_cast<Bangunan*>(temp)){
             cout << " (" << Toko::catalogue[i].second << ")";
         }
